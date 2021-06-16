ab0907e9be7390821e3d6beb9017da79391e1a65
Volinr1 <83939381+Volinr1@users.noreply.github.com>
2021-06-16 16:13:19 +0300

    first commit!

---
+++ assets/js/server.js
@@ -0,0 +1,13 @@
+var url = "https://api.mcsrvstat.us/2/ecpehub.net:19132";
+$.getJSON(url, function(r) {
+ if(r.error){
+    $('h2').html('Server Offline');
+    $('#rest').html('Players Online: 0');
+   return false;
+       } else {
+      $('h2').html('tws-mc.com');  
+      $('#rest').html('Players Online: '+r.players.online+'/'+r.players.max);
+ }   
+});
+
+
